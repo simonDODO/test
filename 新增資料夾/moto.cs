7c5fd0398f7fd8fe7082b7886fdd614527730293
simonDODO <74124127+simonDODO@users.noreply.github.com>
2020-11-15 17:13:46 +0800

    11/15

---
+++ 新增資料夾/moto.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moto : MonoBehaviour
+{
+    // 欄位 field:儲存資料
+    // 欄位語法
+    // 修飾詞 欄位類型 欄位名稱 (指定 值) 結束符號
+    // 欄位值是以 unity 屬性 (inspector)面板為主
+
+    //整數 int:沒有小數點的值 ， 例如:1 - 100 ， -999
+    //浮點數 float:有小數點的值， 例如:0.123 或-9.999
+    //字串 string: 文字， 藥水 ‧ abcde  必須在""內
+    //布林值 bool:是，否 例:true 或 false(預設值是否)
+
+    //修飾詞:預設為私人
+    //私人 private :不顯示
+    //公開 public : 顯示
+
+    //資料
+    //品牌，cc數，重量，是否有天窗，速度，顏色...等等
+
+    public string Brand = "toyota";
+    public int cc = 2000;
+    public float weight = 1.4f;
+    public bool window = true;
+    
+
+
+
+
+
+
+
+}  
---
+++ 新增資料夾/動作分割與腳本_練習/Flyboard_Air/Move.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+	public bool Run;
+    public float RotateSpeed = 30;
+    public Animator PlayAnim;
+
+	void Start ()
+    {
+        // 得到角色的動畫控制器
+        //PlayAnim = GetComponent<Animator>();
+	}
+	
+	void Update ()
+    {
+        // 播放角色動畫(New Bool 判斷是否要 Fly )
+        PlayAnim.SetBool("New Bool", Run);
+        // 如果有按下方向鍵或sd執行
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") == 1)
+        {
+            // 滑板飛起來
+            Run = true;
+            // 往右按會往右飛
+            if (Input.GetAxisRaw("Horizontal") >= 1)
+                transform.Rotate(new Vector3(0, RotateSpeed * Time.deltaTime, 0));
+            // 往左按會往左飛
+            else if (Input.GetAxisRaw("Horizontal") <= -1)
+                transform.Rotate(new Vector3(0, -RotateSpeed * Time.deltaTime, 0));
+        }
+        else
+        {
+            // 滑板不要飛起來
+            Run = false;
+        }
+	}
+}
